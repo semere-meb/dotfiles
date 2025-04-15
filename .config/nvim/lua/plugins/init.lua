4a4db5c8787fea018a8d2343cd866e40359ad382
Semere Mebrahtom <semere.meharena1@gmail.com>
2025-04-15 03:08:23 +0200

    update: tmux config, tmux with nvim, disabled caps lock

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,104 +1,108 @@
 return {
 
-	{
-		"mfussenegger/nvim-dap",
-		config = function()
-			require "configs.dap"
-		end,
-	},
+    {
+        "mfussenegger/nvim-dap",
+        config = function()
+            require "configs.dap"
+        end,
+    },
 
-	{
-		"nvim-neotest/nvim-nio",
-	},
+    {
+        "nvim-neotest/nvim-nio",
+    },
 
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		event = "VeryLazy",
-		config = function()
-			require "configs.mason-dap"
-		end,
-	},
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        event = "VeryLazy",
+        config = function()
+            require "configs.mason-dap"
+        end,
+    },
 
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"nvim-neotest/nvim-nio",
-		},
-		config = function()
-			require "configs.dap-ui"
-		end,
-	},
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "nvim-neotest/nvim-nio",
+        },
+        config = function()
+            require "configs.dap-ui"
+        end,
+    },
 
-	{
-		"mfussenegger/nvim-dap-python",
-		ft = "python",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"rcarriga/nvim-dap-ui",
-		},
-		config = function()
-			require "configs.dap-python"
-		end,
-	},
+    {
+        "mfussenegger/nvim-dap-python",
+        ft = "python",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            "rcarriga/nvim-dap-ui",
+        },
+        config = function()
+            require "configs.dap-python"
+        end,
+    },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require "configs.treesitter"
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require "configs.treesitter"
+        end,
+    },
 
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require("nvchad.configs.lspconfig").defaults()
-			require "configs.lspconfig"
-		end,
-	},
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("nvchad.configs.lspconfig").defaults()
+            require "configs.lspconfig"
+        end,
+    },
 
-	{
-		"williamboman/mason-lspconfig.nvim",
-		event = "VeryLazy",
-		dependencies = { "nvim-lspconfig" },
-		config = function()
-			require "configs.mason-lspconfig"
-		end,
-	},
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = "VeryLazy",
+        dependencies = { "nvim-lspconfig" },
+        config = function()
+            require "configs.mason-lspconfig"
+        end,
+    },
 
-	{
-		"mfussenegger/nvim-lint",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			require "configs.lint"
-		end,
-	},
+    {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require "configs.lint"
+        end,
+    },
 
-	{
-		"stevearc/conform.nvim",
-		event = "BufWritePre",
-		config = function()
-			require "configs.conform"
-		end,
-	},
+    {
+        "stevearc/conform.nvim",
+        event = "BufWritePre",
+        config = function()
+            require "configs.conform"
+        end,
+    },
 
-	{
-		"zapling/mason-conform.nvim",
-		event = "VeryLazy",
-		dependencies = { "conform.nvim" },
-		config = function()
-			require "configs.mason-conform"
-		end,
-	},
+    {
+        "zapling/mason-conform.nvim",
+        event = "VeryLazy",
+        dependencies = { "conform.nvim" },
+        config = function()
+            require "configs.mason-conform"
+        end,
+    },
 
-	{
-		"rshkarin/mason-nvim-lint",
-		event = "VeryLazy",
-		dependencies = { "nvim-lint" },
-		config = function()
-			require "configs.mason-lint"
-		end,
-	},
+    {
+        "rshkarin/mason-nvim-lint",
+        event = "VeryLazy",
+        dependencies = { "nvim-lint" },
+        config = function()
+            require "configs.mason-lint"
+        end,
+    },
+    {
+        "christoomey/vim-tmux-navigator",
+        lazy = false,
+    },
 }
