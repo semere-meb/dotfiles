5b77966886a4f9a37e8c67ad2b95e7768aac4e2a
Semere Mebrahtom <semere.meharena1@gmail.com>
2025-05-05 23:51:19 +0200

    neovim config from scratch -- kickstart-based, zathura clipboard fix, kitty set as default term in hyprland

--- .config/nvim/lua/configs/yazi.lua
+++
@@ -1,8 +0,0 @@
-require("yazi").setup {
-    open_for_directories = false, -- open yazi if opening a directory
-}
-
-local map = vim.keymap.set
-map({ "n", "v" }, "<leader>-", "<cmd>Yazi<cr>", { desc = "Open yazi at the current file" })
-map("n", "<leader>cw", "<cmd>Yazi cwd<cr>", { desc = "Open the file manager in nvim's working directory" })
-map("n", "<c-up>", "<cmd>Yazi toggle<cr>", { desc = "Resume the last yazi session" })
